a4b9967ee1d18db762d7a10752592479ace2f412
John Murphy-Teixidor <johnvmt@gmail.com>
2016-08-16 23:07:48 -0400

    Virtual Link now working

--- VirtualLink.js
+++ VirtualLink.js
@@ -4,10 +4,55 @@
 
 var EventEmitter = require('event-emitter');
 
-function VirtualLink() {
+function VirtualConnection() {
+	var connection = this;
+	connection._emitter = EventEmitter({});
+	connection.connected = false;
+	connection._connectionRequested = false;
 
-	var connection1 = this._newConnection();
-	var connection2 = this._newConnection();
+	connection.on('connected', function() {
+		connection.connected = true;
+	});
+
+	connection.on('disconnected', function() {
+		connection.connected = false;
+	});
+}
+
+VirtualConnection.prototype.connect = function(callback) {
+	this._connectionRequested = true;
+	if(this.connected)
+		callback();
+	else {
+		this.once('connected', callback);
+		this._emitter.emit('connectionRequest');
+	}
+};
+
+VirtualConnection.prototype.emit = function(data) {
+	this._emitter.emit('transmit', data);
+};
+
+VirtualConnection.prototype.on = function(eventType, eventCallback) {
+	this._emitter.on(eventType, eventCallback);
+};
+
+VirtualConnection.prototype.once = function(eventType, eventCallback) {
+	this._emitter.once(eventType, eventCallback);
+};
+
+VirtualConnection.prototype.off = function(eventType, eventCallback) {
+	this._emitter.off(eventType, eventCallback);
+};
+
+VirtualConnection.prototype.receive = function(data) {
+	this._emitter.emit('data', data);
+};
+
+module.exports = function() {
+
+	var connection1 = new VirtualConnection();
+	var connection2 = new VirtualConnection();
 
 	connection1.on('transmit', function(messageType, messageData) {
 		connection2.receive(messageType, messageData);
@@ -17,38 +62,19 @@ function VirtualLink() {
 		connection1.receive(messageType, messageData);
 	});
 
-	this.connection1 = connection1;
-	this.connection2 = connection2;
-}
-
-VirtualLink.prototype._newConnection = function () {
-	return {
-		_emitter: EventEmitter({}),
-		status: 'starting',
-		connect: function(callback) {
-			if(this.status == 'ready')
-				callback();
-			else
-				this.once('ready', callback);
-		},
-		on: function(eventType, eventCallback) {
-			this._emitter.on(eventType, eventCallback);
-		},
-		once: function(eventType, eventCallback) {
-			this._emitter.once(eventType, eventCallback);
-		},
-		off: function(eventType, eventCallback) {
-			this._emitter.off(eventType, eventCallback);
-		},
-		emit: function(data) {
-			this._emitter.emit('transmit', data);
-		},
-		receive: function(data) {
-			this._emitter.emit('data', data);
+	connection2.on('connectionRequest', function() {
+		if(connection1._connectionRequested) {
+			connection1._emitter.emit('connected');
+			connection2._emitter.emit('connected');
 		}
-	};
-};
+	});
 
-module.exports = function() {
-	return new VirtualLink()
+	connection1.on('connectionRequest', function() {
+		if(connection2._connectionRequested) {
+			connection1._emitter.emit('connected');
+			connection2._emitter.emit('connected');
+		}
+	});
+
+	return {connection1: connection1, connection2: connection2};
 };
